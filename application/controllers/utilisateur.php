becab14578780b174c8749de00a15869d542bda8
Mandresyy <aandriahenintsoamandresy@gmail.com>
2023-02-07 21:37:54 +0100

    vue login

--- application/controllers/utilisateur.php
+++ application/controllers/utilisateur.php
@@ -4,13 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class utilisateur extends CI_Controller {
     
     public function login(){
-        $this->load->view('template/login');   
+        $this->load->helper('url');
+        $this->load->view('template/login');  
     }
 
     public function signIn(){
-        $this->laod->helper(url);
+        $this->load->helper(url);
         $this->load->view('template/signin');
     }
+
+
+
 
 
 
