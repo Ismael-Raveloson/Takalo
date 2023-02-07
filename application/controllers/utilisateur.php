2e85e2767dd6b94a9862f89a8dfadda40bbd84b4
@Ismael-Raveloson <isma.raveloson@gmail.com>
2023-02-07 21:31:14 +0300

    Tohiany Controller/Model

--- application/controllers/utilisateur.php
+++ application/controllers/utilisateur.php
@@ -1,12 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class dBController extends CI_Controller {
+class utilisateur extends CI_Controller {
+    
     public function login(){
         $this->load->view('template/login');   
     }
 
     public function signIn(){
+        $this->laod->helper(url);
         $this->load->view('template/signin');
     }
 
